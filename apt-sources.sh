12a31e8c0b6b3039cafdb306c727452313b5fe05
Oscar Olsson <osse.olsson@gmail.com>
2018-05-07 13:46:29 +0200

    Update open forti GUI source

--- apt-sources.sh
+++ apt-sources.sh
@@ -5,15 +5,27 @@ SETUP_PATH=$(dirname "$SCRIPT")
 
 REL_NAME=$(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=)
 
+get_lts_rel()
+{
+    case "$1" in
+      "bionic") LTS_REL_NAME="bionic" ;;
+      "astral") LTS_REL_NAME="xenial" ;;
+      "zesty") LTS_REL_NAME="xenial" ;;
+      *) LTS_REL_NAME="xenial" ;;
+    esac
+}
+
+get_lts_rel $REL_NAME
+
 ### BEGIN i3 setup
 /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb /tmp/sur5r-keyring_2018.01.30_all.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
 dpkg -i /tmp/sur5r-keyring_2018.01.30_all.deb
 echo "deb http://debian.sur5r.net/i3/ $REL_NAME universe" > /etc/apt/sources.list.d/sur5r-i3.list
 ### END i33 setup
 
-### BEGIN OpenFortiGUI
+### BEGIN OpenFortiGUI https://hadler.me/linux/openfortigui/
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2FAB19E7CCB7F415
-echo "deb https://apt.iteas.at/iteas $REL_NAME main" > /etc/apt/sources.list.d/iteas.list
+echo "deb https://apt.iteas.at/iteas $LTS_REL_NAME main" > /etc/apt/sources.list.d/iteas.list
 ### END
 
 ### Docker
