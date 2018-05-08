572ad0310a6c568a110857f98dae7c2f58ad4163
Oscar Olsson <osse.olsson@gmail.com>
2018-05-08 14:49:43 +0200

    Fix ssh host variable

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p .ssh
+HOST=$(hostnamectl status | grep hostname | cut -f2 -d':' | grep -o '\S\+')
 if [ ! -f ~/.ssh/id_rsa ]; then
   ssh-keygen -t rsa -b 4096 -C "$USER@$HOST" -f ~/.ssh/id_rsa -N ""
 fi
