4632bbd4d4141458736ba7a2bb3cbbf8e549eeea
Oscar Olsson <osse.olsson@gmail.com>
2018-05-08 14:50:21 +0200

    Fix install pkgs with sudo

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get update && apt-get install -y $(cat packages.list)
+sudo apt-get update && sudo apt-get install -y $(cat packages.list)
 
 ### docker
 sudo usermod -aG docker $USER
