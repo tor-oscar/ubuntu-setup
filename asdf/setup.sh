e3ce0422bc8746f94af38148ced30d3c4580afaf
Oscar Olsson <osse.olsson@gmail.com>
2018-05-08 14:50:53 +0200

    Properly source asdf

--- asdf/setup.sh
+++ asdf/setup.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.3
 
-. ~/.asdf/asdf.sh
+source ~/.asdf/asdf.sh
 
 asdf plugin-add erlang # https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir # https://github.com/asdf-vm/asdf-elixir.git
